0f4176982ed66d202b046be58bd5859d84c47bfd
gkcodez <gopalakrishnan.work@gmail.com>
2025-02-15 10:01:05 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,196 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { HeartIcon, FolderOpen, Edit, Save } from "lucide-react";
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
+
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [cards, setCards] = useState<
+    {
+      id: number;
+      title: string;
+      description: string;
+      value: string;
+      favorite: boolean;
+    }[]
+  >([]);
+  const [target, setTarget] = useState<string>("example.com")
+  const [isUpdatingTarget, setIsUpdatingTarget] = useState<boolean>(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+  useEffect(() => {
+    fetch("/data/dorks.json")
+      .then((res) => res.json())
+      .then((data) => setCards(data))
+      .catch((error) => console.error("Error loading cards:", error));
+  }, []);
+
+  function googleSearch(searchTerm: string) {
+    if(target) {
+      searchTerm = `site: ${target} ${searchTerm}`
+    }
+    const baseUrl = "https://www.google.com";
+    const url = `${baseUrl}/search?q=${encodeURIComponent(searchTerm)}`;
+    window.open(url, "_blank");
+  }
+
+  const handleTargetChange = (event) => {
+    setTarget(event.target.value);
+  };
+
+  const toggleFavorite = (event) => {
+    console.log(event);
+    
+  }
+
+  const handleEditButtonClick = () => {
+    setIsUpdatingTarget(true);
+  }
+
+  const handleSaveButtonClick = () => {
+    setIsUpdatingTarget(false);
+  }
+
+
+  return (
+    <div className="flex flex-col items-center justify-items-center gap-3 w-full h-full p-10">
+                {/* <Image
+            src="/target.svg" // Replace with your image
+            alt="Target Image"
+            width={300}
+            height={300}
+            className="rounded-lg"
+          /> */}
+      <div className="flex flex-col items-center justify-items-center gap-2 w-full">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-4xl font-bold tracking-tight lg:text-5xl text-indigo-600">
+            Dorkmine
+          </h1>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="http://linkedin.com/in/gkcodez"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            <p className="text-gray-500">
+              Developed by{" "}
+              <span className="text-red-600 underline">@gkcodez</span>
+            </p>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-start gap-2 flex-1 w-full p-2">
+            <div className="flex items-center justify-center gap-2">
+              <span className="font-bold">Target: </span>{" "}
+              { !isUpdatingTarget && 
+              <div className="flex items-center gap-2">
+              <span
+                id="currentTarget"
+                className="text-red-600"
+              >
+                {target}
+              </span>
+              <Edit onClick={handleEditButtonClick}/>
+              </div>
+              }
+              {
+                isUpdatingTarget && 
+                <div className="flex items-center gap-2">
+                <Input value={target} onChange={handleTargetChange}/>
+                <Save onClick={handleSaveButtonClick}/>
+
+                </div>
+              }
+            </div>
+          </div>
+          <div className="flex gap-2 flex-1 w-full lg:w-1/3 p-2">
+            <Input placeholder="Search dorks by title" />
+            <Button className="bg-indigo-600 hover:bg-indigo-800 text-white">
+              Search
+            </Button>
+          </div>
+          <div className="favoriteDorks p-2">
+            <div className="flex items-center justify-items-center p-2 w-full gap-2">
+              <h3 className="flex items-center gap-2 text-left text-xl font-bold tracking-tight lg:text-2xl text-indigo-600">
+                <FolderOpen /> Favorites <FolderOpen />
+              </h3>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6 gap-2 p-2 w-full">
+              {cards
+                .filter((x) => x.favorite)
+                .map((card, index) => (
+                  <Card
+                    key={index}
+                    className="shadow-md border border-gray-200 hover:shadow-lg transition"
+                  >
+                    <div onClick={() => googleSearch(card.value)}>
+                      <CardHeader>
+                        <CardTitle className="text-lg font-normal">
+                          {card.title}
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent>
+                        <p className="text-gray-600">{card.description}</p>
+                      </CardContent>
+                    </div>
+                    <CardFooter className="flex items-center justify-end w-full">
+                      <HeartIcon
+                        onClick={() => toggleFavorite(card)}
+                        className="fill-red-600 text-red-500"
+                      />
+                    </CardFooter>
+                  </Card>
+                ))}
+            </div>
+          </div>
+          <div className="allDorks p-2">
+            <div className="flex items-center justify-items-center p-2 w-full">
+              <h3 className="flex items-center gap-2 text-left text-xl font-bold tracking-tight lg:text-2xl text-indigo-600">
+                <FolderOpen /> All Dorks
+              </h3>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-6 gap-2 p-2 w-full">
+              {cards.map((card, index) => (
+                <Card
+                  key={index}
+                  className="shadow-md border border-gray-200 hover:shadow-lg transition"
+                >
+                  <div onClick={() => googleSearch(card.value)}>
+                    <CardHeader>
+                      <CardTitle className="text-lg font-normal">
+                        {card.title}
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <p className="text-gray-600">{card.description}</p>
+                    </CardContent>
+                  </div>
+                  <CardFooter className="flex items-center justify-end w-full">
+                    <HeartIcon onClick={() => toggleFavorite(card)} />
+                  </CardFooter>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
