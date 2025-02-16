92d3f4eaa11a3cfbd391210d90224a03210d2946
gkcodez <gopalakrishnan.work@gmail.com>
2025-02-16 11:39:05 +0530

    Params code added.

--- app/api/dorks/[id]/toggleFavorites/route.ts
+++ app/api/dorks/[id]/toggleFavorites/route.ts
@@ -2,10 +2,9 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const params = await context.params;
-    const { id } = params;
+      const { id } = params;
   
       if (!id) {
         return NextResponse.json({ error: "ID is required" }, { status: 400 });
