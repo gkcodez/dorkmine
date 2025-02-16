6ab2b41068bd89c6b748351ed5b91c7a82ff20b5
gkcodez <gopalakrishnan.work@gmail.com>
2025-02-16 11:54:37 +0530

    Updated context to any

--- app/api/dorks/[id]/toggleFavorites/route.ts
+++ app/api/dorks/[id]/toggleFavorites/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
+export async function PATCH(req: NextRequest, context: any) {
   try {
       const { id } = context.params;
   
