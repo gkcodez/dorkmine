31aa270223a72725088e7206beac9e28f79d06ce
gkcodez <gopalakrishnan.work@gmail.com>
2025-02-26 02:35:07 +0530

    Favicon and icons updated.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Dorkmine",
   description: "Bug bounty dork engine",
+  icons: {
+    icon: "/favicon.svg"
+  },
 };
 
 export default function RootLayout({
